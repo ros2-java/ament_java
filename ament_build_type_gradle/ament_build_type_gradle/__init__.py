2202a50b997d737d0749f74c141a6c21c6a6673a
Esteve Fernandez <esteve@apache.org>
2016-08-22 21:00:52 +0200

    Added ament_build_type_gradle

---
+++ ament_build_type_gradle/ament_build_type_gradle/__init__.py
@@ -0,0 +1,116 @@
+# Copyright 2014 Open Source Robotics Foundation, Inc.
+# Copyright 2016 Esteve Fernandez <esteve@apache.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Implements the BuildType support for gradle based ament packages."""
+
+import os
+import shutil
+
+from ament_tools.helper import extract_argument_group
+
+from ament_tools.build_type import BuildAction
+from ament_tools.build_type import BuildType
+
+IS_WINDOWS = os.name == 'nt'
+
+def get_gradle_executable():
+    gradle_script = 'gradle.bat' if IS_WINDOWS else 'gradle'
+    if 'GRADLE_HOME' in os.environ:
+        gradle_home = os.environ['GRADLE_HOME']
+        gradle_path = os.path.join(gradle_home, 'bin', gradle_script)
+        if os.path.isfile(gradle_path):
+            return gradle_path
+    return shutil.which(gradle_script)
+
+
+GRADLE_EXECUTABLE = get_gradle_executable()
+
+
+class AmentGradleBuildType(BuildType):
+    build_type = 'ament_gradle'
+    description = "ament package built with Gradle"
+
+    def prepare_arguments(self, parser):
+        parser.add_argument(
+            '--ament-gradle-args',
+            nargs='*',
+            default=[],
+            help="Arbitrary arguments which are passed to 'ament_gradle' Gradle projects. "
+                 "Argument collection can be terminated with '--'.")
+
+    def argument_preprocessor(self, args):
+        # The ament CMake pass-through flag collects dashed options.
+        # This requires special handling or argparse will complain about
+        # unrecognized options.
+        args, gradle_args = extract_argument_group(args, '--ament-gradle-args')
+        extras = {
+            'ament_gradle_args': gradle_args,
+        }
+        return args, extras
+
+    def extend_context(self, options):
+        ce = super(AmentGradleBuildType, self).extend_context(options)
+        ce.add('ament_gradle_args', options.ament_gradle_args)
+        return ce
+
+    def on_build(self, context):
+        cmd_args = [
+            '-Pament.build_space=' + context.build_space,
+            '-Pament.install_space=' + context.install_space,
+            '-Pament.dependencies=' + ':'.join(context.build_dependencies),
+        ]
+        cmd_args += context.ament_gradle_args
+
+        cmd = [GRADLE_EXECUTABLE]
+        cmd += cmd_args
+        cmd += ['build']
+
+        yield BuildAction(cmd, cwd=context.source_space)
+
+    def on_test(self, context):
+        cmd_args = [
+            '-Pament.build_space=' + context.build_space,
+            '-Pament.install_space=' + context.install_space,
+            '-Pament.dependencies=' + ':'.join(context.build_dependencies),
+        ]
+        cmd_args += context.ament_gradle_args
+
+        cmd = [GRADLE_EXECUTABLE]
+        cmd += cmd_args
+        cmd += ['test']
+
+        yield BuildAction(cmd, cwd=context.source_space)
+
+    def on_install(self, context):
+        cmd = [
+            GRADLE_EXECUTABLE,
+            '-Pament.build_space=' + context.build_space,
+            '-Pament.install_space=' + context.install_space,
+            '-Pament.dependencies=' + ':'.join(context.build_dependencies),
+            'build',
+        ]
+
+        yield BuildAction(cmd, cwd=context.source_space)
+
+    def on_uninstall(self, context):
+        cmd = [
+            GRADLE_EXECUTABLE,
+            '-Pament.build_space=' + context.build_space,
+            '-Pament.install_space=' + context.install_space,
+            '-Pament.dependencies=' + ':'.join(context.build_dependencies),
+            'clean',
+        ]
+
+        yield BuildAction(cmd, cwd=context.source_space)
---
+++ ament_build_type_gradle/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='ament_build_type_gradle',
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    install_requires=['ament-package', 'osrf_pycommon'],
+    author='Esteve Fernandez',
+    author_email='esteve@apache.org',
+    maintainer='Esteve Fernandez',
+    maintainer_email='esteve@apache.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Gradle tool support for ament.',
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    entry_points={
+        'ament.build_types': [
+            'ament_gradle = ament_build_type_gradle:AmentGradleBuildType',
+        ],
+    }
+)
