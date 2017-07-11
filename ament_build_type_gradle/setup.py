e0808a74469ca4287a44a60b93846ccbb89f7494
Esteve Fernandez <esteve@apache.org>
2017-07-11 18:52:10 +0200

    Use ament_java_resources to store CLASSPATH templates

--- ament_build_type_gradle/setup.py
+++ ament_build_type_gradle/setup.py
@@ -2,69 +2,6 @@ import os
 
 from setuptools import find_packages
 from setuptools import setup
-from setuptools.command.install import install
-from setuptools.command.develop import develop
-
-IS_WINDOWS = os.name == 'nt'
-
-# Customize both the install (non-symlinked) and develop (symlinked) commands so that we can
-# install an entry in the ament index for the path to the CLASSPATH templates
-
-
-class ament_gradle_install(install):
-    def run(self):
-        super().run()
-        install_dir = self.prefix
-        install_index_dir = os.path.join(install_dir, 'share', 'ament_index',
-                                         'resource_index', 'templates')
-        install_index_path = os.path.join(install_index_dir,
-                                          'ament_build_type_gradle_classpath')
-
-        install_lib_dir = self.get_finalized_command('install_lib').install_dir
-
-        template_dir = os.path.join(install_lib_dir, 'ament_build_type_gradle',
-                                    'template', 'environment_hook')
-        template_filename = 'classpath' + ('.sh.in'
-                                           if not IS_WINDOWS else '.bat.in')
-        template_path = os.path.join(template_dir, template_filename)
-
-        self.mkpath(install_index_dir)
-        with open(install_index_path, 'w') as f:
-            f.write(template_path)
-
-
-class ament_gradle_develop(develop):
-    def run(self):
-        super().run()
-        build_dir = os.path.abspath(self.setup_path)
-        src_dir = os.path.dirname(
-            os.path.realpath(os.path.join(build_dir, 'setup.py')))
-        install_dir = self.prefix
-
-        template_dir = os.path.join(src_dir, 'ament_build_type_gradle',
-                                    'template', 'environment_hook')
-        template_filename = 'classpath' + ('.sh.in'
-                                           if not IS_WINDOWS else '.bat.in')
-        template_path = os.path.join(template_dir, template_filename)
-
-        build_index_dir = os.path.join(build_dir, 'share', 'ament_index',
-                                       'resource_index', 'templates')
-        build_index_path = os.path.join(build_index_dir,
-                                        'ament_build_type_gradle_classpath')
-        self.mkpath(build_index_dir)
-        with open(build_index_path, 'w') as f:
-            f.write(template_path)
-
-        install_index_dir = os.path.join(install_dir, 'share', 'ament_index',
-                                         'resource_index', 'templates')
-        install_index_path = os.path.join(install_index_dir,
-                                          'ament_build_type_gradle_classpath')
-        self.mkpath(install_index_dir)
-
-        if os.path.exists(install_index_path):
-            os.remove(install_index_path)
-        if not os.path.exists(install_index_path):
-            os.symlink(build_index_path, install_index_path)
 
 
 setup(
@@ -93,7 +30,4 @@ setup(
     package_data={
         'ament_build_type_gradle': ['template/environment_hook/*.in']
     },
-    cmdclass={
-        'develop': ament_gradle_develop,
-        'install': ament_gradle_install,
-    }, )
+    )
